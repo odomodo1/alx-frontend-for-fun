fcf86661d1764ab0812dc4c1430d5224e4a87dd4
odomodo1 <odomodo19@gmail.com>
2023-10-23 04:57:30 -0700

    markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -16,18 +16,18 @@ def convert_markdown_to_html(input_file, output_file):
         print(f"Missing {input_file}", file=sys.stderr)
         sys.exit(1)
 
-        # Read the Markdown file and convert it to HTML
-        with open(input_file, encoding="utf-8") as f:
-            html_lines = []
-            for line in f:
-                # Check for Markdown headings
-                match = re.match(r"^(#+) (.*)$", line)
-                if match:
-                    heading_level = len(match.group(1))
-                    heading_text = match.group(2)
-                    html_lines.append(f"<h{heading_level}>{heading_text}</h{heading_level}>")
-                else:
-                    html_lines.append(line.rstrip())
+     # Read the Markdown file and convert it to HTML
+     with open(input_file, encoding="utf-8") as f:
+         html_lines = []
+         for line in f:
+             # Check for Markdown headings
+             match = re.match(r"^(#+) (.*)$", line)
+             if match:
+                 heading_level = len(match.group(1))
+                 heading_text = match.group(2)
+                 html_lines.append(f"<h{heading_level}>{heading_text}</h{heading_level}>")
+            else:
+                 html_lines.append(line.rstrip())
 
     # Write the HTML output to a file
     with open(output_file, "w", encoding="utf-8") as f:
